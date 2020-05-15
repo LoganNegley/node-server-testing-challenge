21867db4a5f34dd1cb560ec67830299d8bf40f8f
Logan Negley <logannegley@gmail.com>
2020-05-15 13:14:09 -0500

    updated catch on getting all songs routes

--- songs/songs-router.js
+++ songs/songs-router.js
@@ -3,12 +3,15 @@ const router = express.Router();
 
 const db = require('../songs/songs-model');
 
-router.get('/api/songs', (req,res) =>{
+router.get('/', (req,res) =>{
     db.getAll()
     .then(songs =>{
         res.status(200).json(songs)
     })
-    .catch(res.status(500).json({errorMessage: 'Unable to get songs'}))
+    .catch(error =>{
+        console.log(error)
+        res.status(500).json({errorMessage:'Error getting all songs'})
+    })
 })
 
 module.exports = router;
